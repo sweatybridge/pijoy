7e31c6b8816734aee8328cf9c57d60d6ae89a134
Han Qiao <sweatybridge@gmail.com>
2024-06-21 01:01:18 +0800

    chore: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=arm
+export GOARM=6
+
+#go build -trimpath -ldflags '-s -w -X github.com/sweatybridge/pijoy/internal.version=dev'
+#scp ./pijoy raspberrypi.local:/home/qh812
+go build -trimpath -ldflags '-s -w -X github.com/sweatybridge/pijoy/internal.version=dev' tools/debug/main.go
+scp ./main raspberrypi.local:/home/qh812
