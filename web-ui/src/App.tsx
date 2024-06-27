4435bb194e723da0abbc364d96404b54ca1f5cc0
Qiao Han <qiao@supabase.io>
2024-06-27 12:19:14 +0800

    chore: use vite and swc

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-import Joystick from "./components/Joystick";
-import logo from "./logo.svg";
-import "./App.css";
+import reactLogo from './assets/react.svg'
+import Joystick from './components/Joystick'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Joystick className="card" tabIndex={-1}>
         <p>Press direction keys to control the claw.</p>
-        <Joystick className="joystick" tabIndex={-1} />
-      </header>
-    </div>
-  );
+      </Joystick>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
